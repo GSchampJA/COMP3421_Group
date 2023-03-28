e82d1259cbd3f0979f863b96d9ce8c06ba45171e
GSchampJA <jackau1206@gmail.com>
2023-03-28 22:16:43 +0800

    refined database and updated signup.php

--- database.sql
+++ database.sql
@@ -1,33 +1,51 @@
-DROP TABLE USER;
-DROP TABLE ACCESS_RECORD;
-DROP TABLE POST_RECORD;
-DROP TABLE COMMENT_RECORD;
+-- DROP TABLE `user`;
+-- DROP TABLE `access_record`;
+-- DROP TABLE `post_record`;
+-- DROP TABLE `comment_record`;
 
-CREATE TABLE USER(UserID NUMBER(5) NOT NUll, username VARCHAR (30) NOT NULL, password VARCHAR(30) NOT NULL);
-ALTER TABLE USER ADD PRIMARY KEY (UserID);
+CREATE TABLE `user`(UserID int NOT NUll, Username VARCHAR (100) NOT NULL, Password VARCHAR(100) NOT NULL, PRIMARY KEY(UserID));
 
-CREATE TABLE POST_RECORD(PostTYPE TEXT (65535) NOT NULL, PostID int NOT NULL, PostNAME TEXT (100) NOT NULL, PostCONTENT TEXT (65535) NOT NULL, PostTIME TIME() NOT NULL, FOREIGN KEY (UserID) REFERENCES USER(UserID));
-ALTER TABLE CUSTOMER ADD PRIMARY KEY (PostID);
+CREATE TABLE `post_record`(PostID int NOT NULL, Posttype TEXT (65535) NOT NULL, Postname TEXT (100) NOT NULL, Postcontent TEXT (65535) NOT NULL, Postimage longblob NOT NULL, Posttime TIMESTAMP NOT NULL, UserID int NOT NULL, FOREIGN KEY (UserID) REFERENCES user(UserID));
+ALTER TABLE `post_record` ADD PRIMARY KEY (PostID);
 
-CREATE TABLE ACCESS_RECORD(TIME(), FOREIGN KEY (PostID) REFERENCES POST_RECORD(PostID), FOREIGN KEY (UserID) REFERENCES USER(UserID));
+CREATE TABLE `access_record`(Accesstime TIMESTAMP NOT NULL, PostID int NOT NULL,FOREIGN KEY (PostID) REFERENCES post_record(PostID), UserID int NOT NUll, FOREIGN KEY (UserID) REFERENCES user(UserID));
 
-CREATE TABLE COMMENT_RECORD(TIME(), FOREIGN KEY (PostID) REFERENCES POST_RECORD(PostID), FOREIGN KEY (UserID) REFERENCES USER(UserID), PostCONTENT TEXT (65535) NOT NULL, PostTIME TIME() NOT NULL);
-ALTER TABLE COMMENT_RECORD ADD PRIMARY KEY (COMID);
+CREATE TABLE `comment_record`(CommentID int NOT NULL, PostID int NOT NULL ,FOREIGN KEY (PostID) REFERENCES post_record(PostID), UserID int NOT NUll, FOREIGN KEY (UserID) REFERENCES user(UserID), Commentcontent TEXT (65535) NOT NULL,Commenttime TIMESTAMP NOT NULL);
+ALTER TABLE `comment_record` ADD PRIMARY KEY (CommentID);
 
--- table customer
-INSERT INTO CUSTOMER VALUES (00001, 'Adams Willsink', 'DIls2223ed');
-INSERT INTO CUSTOMER VALUES (00002, 'William Felix', 'djHJ-0dc~','M');
-INSERT INTO CUSTOMER VALUES (00003, 'Brett Howe', 'jdkkUDDUvh1~~','M');
-INSERT INTO CUSTOMER VALUES (00004, 'Raphael Gilbert', '-iofDWA8iedjj','M');
-INSERT INTO CUSTOMER VALUES (00005, 'Troy Molina', 'coVBHDgg34o','F');
-INSERT INTO CUSTOMER VALUES (00006, 'Adams Nosan', 'jkOP93!oif0=','M');
-INSERT INTO CUSTOMER VALUES (00007, 'Tom Johns', 'djhuiu2PL2222','M');
-INSERT INTO CUSTOMER VALUES (00008, 'Roosevelt Savage', 'di2334snisia','M');
-INSERT INTO CUSTOMER VALUES (00009, 'Conner Lynch', 'kdLks88xxp-','M');
-INSERT INTO CUSTOMER VALUES (00010, 'Haider Lester', 'dklk---dxLO','M');
-INSERT INTO CUSTOMER VALUES (00011, 'Jack Au', 'Jack123!!','M');
-INSERT INTO CUSTOMER VALUES (00012, 'Daniel Yu', 'Daniel456~~','M');
-INSERT INTO CUSTOMER VALUES (00013, 'Winnie', 'Winnie789-=','M');
-INSERT INTO CUSTOMER VALUES (00014, 'Gabriel', 'Gabriel101','M');
+-- table user
+INSERT INTO user VALUES (00001, 'Adams Willsink', 'DIls2223ed');
+INSERT INTO user VALUES (00002, 'William Felix', 'djHJ-0dc~');
+INSERT INTO user VALUES (00003, 'Brett Howe', 'jdkkUDDUvh1~~');
+INSERT INTO user VALUES (00004, 'Raphael Gilbert', '-iofDWA8iedjj');
+INSERT INTO user VALUES (00005, 'Troy Molina', 'coVBHDgg34o');
+INSERT INTO user VALUES (00006, 'Adams Nosan', 'jkOP93!oif0=');
+INSERT INTO user VALUES (00007, 'Tom Johns', 'djhuiu2PL2222');
+INSERT INTO user VALUES (00008, 'Roosevelt Savage', 'di2334snisia');
+INSERT INTO user VALUES (00009, 'Conner Lynch', 'kdLks88xxp-');
+INSERT INTO user VALUES (00010, 'Haider Lester', 'dklk---dxLO');
+INSERT INTO user VALUES (00011, 'Jack Au', 'Jack123!!');
+INSERT INTO user VALUES (00012, 'Daniel Yu', 'Daniel456~~');
+INSERT INTO user VALUES (00013, 'Winnie', 'Winnie789-=');
+INSERT INTO user VALUES (00014, 'Gabriel', 'Gabriel101');
 
--- table access record
+-- table post record
+INSERT INTO post_record VALUES (00001, 'PolyU Life', 'Handsome guy Gabriel', 'Gabriel, the most handsome guy in comp department, trust me!', 0,'2023-03-25 07:52:11', 00014);
+INSERT INTO post_record VALUES (00002, 'Faculty News', 'Winnie is getting Microsoft Azure Offer!!!', 'Congrats', 0,'2023-03-25 09:30:55', 00005);
+INSERT INTO post_record VALUES (00003, 'PolyU Life', 'I am getting 1.7 gpa', 'Who can carry me!!!!!!!', 0,'2023-03-25 09:52:46', 00011);
+INSERT INTO post_record VALUES (00004, 'PolyU Life', 'Daniel Yu is nto having offer from microsoft', 'Can someone comfort me...', 0,'2023-03-25 10:56:55', 00012);
+
+-- table access record 
+INSERT INTO access_record VALUES('2023-03-25 11:30:55', 4, 1);
+INSERT INTO access_record VALUES('2023-03-25 12:30:23', 2, 3);
+INSERT INTO access_record VALUES('2023-03-25 13:45:22', 1, 9);
+INSERT INTO access_record VALUES('2023-03-25 16:35:12', 3, 10);
+INSERT INTO access_record VALUES('2023-03-25 18:40:02', 4, 9);
+INSERT INTO access_record VALUES('2023-03-26 01:24:34', 4, 11);
+INSERT INTO access_record VALUES('2023-03-26 02:26:13', 3, 12);
+INSERT INTO access_record VALUES('2023-03-27 17:59:05', 1, 14);
+
+-- table comment record
+INSERT INTO comment_record VALUES(1, 4, 11, 'No byebye', '2023-03-26 01:29:58');
+INSERT INTO comment_record VALUES(2, 3, 12, 'hehe', '2023-03-26 02:28:41');
+INSERT INTO comment_record VALUES(3, 1, 14, 'lets goooooooo', '2023-03-27 18:02:56');
--- signup.php
+++ signup.php
@@ -1,1 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Create a post</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 
+    <!--Add Bootstrap framework into the HTML file-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.3/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    <!---->
+</head>
+
+<body>
+    <div class="container-fluid" style="margin-top:100px">
+        <div class="postForm">
+            <div class="row justify-content-center">
+                <div class="col-sm-4">
+                    <a href="index.html" id="closeButton"><i class="material-icons md-24">close</i></a>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-sm-4" id="createTitle">
+                    <h2>Sign up to PolyU Forum~</h2>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-sm-4">
+                    <form action="#.php" class="was-validated">
+                        <div class="form-group">
+                            <h6>Name:</h6>
+                            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Input here..." required>
+                        </div>
+                        <div class="form-group">
+                            <h6>Password:</h6>
+                            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Input here..." required>
+                        </div>
+                        
+                        <button type="submit" class="btn btn-secondary">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <div class="row justify-content-center">
+            <div class="col-sm-4" style = "text-align: center;">
+                <h6>Or do you have an account already? Log In <a href="login.php">Here</a></h6>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
