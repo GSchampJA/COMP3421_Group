4c08626091b068263cccf7c26791eb9454238923
GSchampJA <jackau1206@gmail.com>
2023-04-14 14:12:18 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,3 +1,8 @@
+<?php 
+    session_start();
+    include 'DBconnection.php';
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -21,29 +26,28 @@
 }
 </style>
 <body>
+    <script src="script.js"></script>   
     <div class="loginbox">
         <img src="images/loginlogo.png" class="icon">
         <?php
-        include 'DBconnection.php';
-
-        if(isset($_POST['login'])){
-           $name = mysqli_real_escape_string($conn, $_POST["username"]);
-           $password = $_POST["password"];
-           $sql = "SELECT * FROM user WHERE Username = '$name' && Password = '$password'";
-           $result = mysqli_query($conn, $sql);
-        if(mysqli_num_rows($result) > 0){
-           echo "<p>OK, 2 seconds to home page</p>";
-           echo "<script>
-                setTimeout(function(){
-                    window.location.href = 'index.php'; 
-                },2000);
-                const data = '<?php echo $name; ?>';
-            </script>";
-        }
-        else{
-           echo "<p>Incorrect user name or password!</p>";
-        }
-        }
+            if(isset($_POST['login'])){
+                $name = mysqli_real_escape_string($conn, $_POST["username"]);
+                $password = $_POST["password"];
+                $sql = "SELECT * FROM user WHERE Username = '$name' && Password = '$password'";
+                $result = mysqli_query($conn, $sql);
+            if(mysqli_num_rows($result) > 0){
+                echo "<p>OK, 2 seconds to home page</p>";
+                echo "<script>
+                        setTimeout(function(){
+                            window.location.href = 'index.php'; 
+                        },2000);
+                    </script>";
+                $_SESSION['username']=$name;
+                $_SESSION['loggedin']=true;
+            } else{
+                echo "<p>Incorrect user name or password!</p>";
+            }
+            }
         ?>
         <form action="login.php" method="post">
             <label for="username">Username:</label>
